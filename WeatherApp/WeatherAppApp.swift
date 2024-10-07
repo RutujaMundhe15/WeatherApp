4ecb1b8c1730f8a98c18cb174dcab4a8d7c7f945
Rutuja Mundhe <rutujamundhe15@gmail.com>
2024-10-07 10:28:35 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Rutuja on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Rutuja on 2024-10-07.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
